2575819b9c64cd4335f6496eb06a613fae17bc2e
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-03-16 17:40:47 +0530

    Fixed import issue

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
-import "../styles/Login.css"; // Import custom styles
+import "../styles/login.css"; // Import custom styles
 
 function Login() {
   const [email, setEmail] = useState("");
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
-import "../styles/Signup.css"; // Import custom styles
+import "../styles/signup.css"; // Import custom styles
 
 function Signup() {
   const [username, setUserName] = useState("");
